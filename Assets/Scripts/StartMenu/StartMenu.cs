b30e5886791dbe1b3f05556fc15f780592a7cbf2
Nessx4 <zazz.joe@hotmail.com>
2017-10-13 14:29:32 +0100

    My first commit!

---
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+	public void DesignLevels_Btn()
+	{
+
+	}
+
+	public void PlayLevels_Btn()
+	{
+
+	}
+
+	public void Options_Btn()
+	{
+
+	}
+
+	public void Quit_Btn()
+	{
+		Process.Start("Chrome.exe", "http://www.google.com");
+		Application.Quit();
+	}
+}
