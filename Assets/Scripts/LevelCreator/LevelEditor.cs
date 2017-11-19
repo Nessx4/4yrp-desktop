908cd7d54bcadeee76243ecf0413263507c9b218
daniel-ilett <danielilett@gmail.com>
2017-11-19 18:17:11 +0000

    Level name input field validation works.
    Also started work on a save function for the Level Editor.

---
+++ Assets/Scripts/LevelCreator/LevelEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+using UnityEngine;
+
+public class LevelEditor : MonoBehaviour 
+{
+	public void Save()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/LevelCreator/LevelName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class LevelName : MonoBehaviour 
+{
+	private InputField input;
+
+	private Regex validate = new Regex("[^a-zA-Z0-9 ]");
+
+	private void Start()
+	{
+		input = GetComponent<InputField>();
+	}
+
+	public void UpdateInput()
+	{
+		string text = input.text;
+		text = validate.Replace(text, "");
+		input.text = text;
+	}
+}
