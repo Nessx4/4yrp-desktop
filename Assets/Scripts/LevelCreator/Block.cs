fe9f708daa54f3f6bd89c0ecfa05faa469c8948c
Daniel Ilett <danielilett@gmail.com>
2017-12-04 00:10:29 +0000

    Start Point is now a placeable tile.
    I have to push my stuff every time I switch between my laptop and desktop so I don't lose progress, which is unfortunately all the time.

--- Assets/Scripts/LevelCreator/Block.cs
+++ Assets/Scripts/LevelCreator/Block.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*	A Block is a tile that gets placed in the Level Editor. This is the component
+ *	that is checked for while raycasting to ensure that the thing that gets hit is
+ *	a valid tile entity.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
