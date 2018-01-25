2c1b164ea39d03292b281404f5206b2b75db44c3
daniel-ilett <danielilett@gmail.com>
2018-01-25 16:12:53 +0000

    Added antialiasing filters.
    UI buttons especially look crisper with SMAA (subpixel morphological antialiasing), although this is an effect found on the Unity Asset Store. The main scene camera and UI camera are both supersampled to retain more detail (this especially works well on tiles with small details, such as food). This is original material.

---
+++ Assets/Scripts/Utility/Supersample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Supersample : MonoBehaviour 
+{
+	[SerializeField] 
+	private List<Camera> cameras;
+
+	private const int factor = 2;
+
+	private RenderTexture supersampleRT;
+
+	private void Start()
+	{
+		supersampleRT = new RenderTexture(Screen.width * factor, Screen.height * factor, 24, RenderTextureFormat.ARGB32);
+	}
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		foreach(var cam in cameras)
+		{
+			cam.targetTexture = supersampleRT;
+			cam.Render();
+			cam.targetTexture = null;
+		}
+
+		Graphics.Blit(supersampleRT, dst);
+	}
+}
