1b88a8c2f853fec7451283e09c70f6262b02583c
daniel-ilett <danielilett@gmail.com>
2018-04-19 12:14:53 +0100

    Added level naming UI.
    Begun the backend functionality for saving and loading a level. A static class, LevelManagement, will be used to persist a level ID across scenes. This ID is used by the internal level database to retrieve the correct level data. An ID of -1 indicates a level that has not been saved before. A level will have local IDs for local storage and global IDs if it has been uploaded online. For online purposes, a version ID will also be used so that users have access to the latest version of a level. Global IDs will be a combination of local ID and user ID. Two local databases will be maintained: local levels created by this user, and global levels downloaded from the Internet that have been stored locally.

---
+++ Assets/Scripts/LevelManagement.cs
@@ -0,0 +1,17 @@
+﻿/*	LevelManagement encapsulates all the functionality required for saving
+ *	and loading a level. 
+ *
+ *	Additionally, it keeps track of the level that will next need to be loaded 
+ *	across scenes, for example when a user selects a level from the Browser or 
+ *	creates a new one (an ID of -1 is assigned by default).
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public static class LevelManagement 
+{
+	public static long id;
+}
