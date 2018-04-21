f519fc3ae0bb65832906d475208cb2b38bdf79b1
daniel-ilett <danielilett@gmail.com>
2018-04-21 10:35:06 +0100

    Architecture for Save/Play buttons begun.
    Scripts for opening the naming dialog box and submitting a name and description have been implemented.

--- Assets/Scripts/LevelEditor/SaveDialog/NameDialog.cs
+++ Assets/Scripts/LevelEditor/SaveDialog/NameDialog.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+﻿/*	NameDialog encapsulates the UI dialog asking the user to name their level.
+ */
+using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NameDialog : MonoBehaviour {
+public class NameDialog : MonoBehaviour 
+{
+	[SerializeField] 
+	private List<NameDialogButton> buttons;
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private InputField nameInput;
+
+	[SerializeField]
+	private InputField descriptionField;
+
+	private void Awake()
+	{
+		// Assign each button's reference back to this instance.
+		foreach(var button in buttons)
+			button.nameDialog = this;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// The Confirm or Cancel button has been pressed.
+	public void TakeAction(NameDialogAction action)
+	{
+		switch(action)
+		{
+			case NameDialogAction.CANCEL:
+				break;
+			case NameDialogAction.CONFIRM:
+				break;
+		}
 	}
 }
---
+++ Assets/Scripts/LevelEditor/SaveDialog/NameDialogButton.cs
@@ -0,0 +1,22 @@
+﻿/*	NameDialogButton denotes one of two buttons on the Name Dialog UI, where 
+ *	each button performs an action; Cancel closes the window, while Confirm
+ *	saves the level with the given name. It may also open Play Mode depending
+ *	on other factors.
+ */
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class NameDialogButton : MonoBehaviour 
+{
+	[SerializeField]
+	private NameDialogAction action;
+
+	public NameDialog nameDialog { private get; set; }
+
+	public void Press()
+	{
+		nameDialog.TakeAction(action);
+	}
+}
