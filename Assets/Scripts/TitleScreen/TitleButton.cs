5af4903881e6d34817298aaa10fc8c3a6fbe38b1
daniel-ilett <danielilett@gmail.com>
2018-05-05 16:01:05 +0100

    Added more to Title Screen. Started browser.

---
+++ Assets/Scripts/TitleScreen/TitleButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour 
+{
+	public void MakeLevels()
+	{
+		SceneManager.LoadScene("");
+	}
+
+	public void PlayLevels()
+	{
+		SceneManager.LoadScene("");
+	}
+
+	public void Options()
+	{
+
+	}
+
+	public void Quit()
+	{
+
+	}
+}
