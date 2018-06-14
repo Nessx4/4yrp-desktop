77fcd70b06eb63084738dab0f5a72c1ae74a9d8d
daniel-ilett <danielilett@gmail.com>
2018-06-14 17:54:58 +0100

    Started Database Server linkup.
    Began using C# async stuff to set up communication between the game and a database hosted somewhere on danielilett.com.

---
+++ Assets/Scripts/TestFeatures/LevelUpload.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class LevelUpload : MonoBehaviour 
+{
+	private async void Start()
+	{
+		Debug.Log("Started the thing");
+
+		await PollServer();
+
+		Debug.Log("Ended the thing");
+	}
+
+	private async Task PollServer()
+	{
+		HttpClient client = new HttpClient();
+
+		Task<string> getStringTask = client.GetStringAsync("http://danielilett.com");
+
+		string urlContents = await getStringTask;
+
+		Debug.Log(urlContents);
+
+		return;
+	}
+}
